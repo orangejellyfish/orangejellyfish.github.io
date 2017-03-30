05a1ac2f9910e0382b96858ffadbea31fc00b647
James Allardice <james@allardice.me>
2017-03-30 15:53:26 +0100

    Fancy typed messages on homepage

---
+++ assets/scripts/custom.js
@@ -0,0 +1,13 @@
+$(function () {
+  $('#build-message').typed({
+    strings: [
+      'faster.',
+      'robustly.',
+      'for high availability.',
+      'passionately.',
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backDelay: 1000,
+  });
+});
